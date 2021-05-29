ae81f473c8f2e863af717212677b5dac898b45f6
françois hiriart <f.hiriart@skema.edu>
2021-05-29 11:36:41 +0200

    tables

---
+++ db/migrate/20210527185353_add_fields_to_users.rb
@@ -0,0 +1,12 @@
+class AddFieldsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :company_name, :string
+    add_column :users, :address, :text
+    add_column :users, :business_activity, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :user_name, :string
+    add_column :users, :role, :string
+    add_column :users, :vat_number, :string
+    add_column :users, :siret_number, :string
+  end
+end
