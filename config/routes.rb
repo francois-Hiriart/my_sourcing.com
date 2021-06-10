1d6f90dc3f603e8e87496fed15176e6c936d2bbd
françois hiriart <f.hiriart@skema.edu>
2021-06-10 19:05:48 +0200

    fix_back I

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :orders
+  resources :orders, except: [:new, :create]
 
   resources :products, only: [:index, :show]
 
