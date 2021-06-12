51ff4218fc0fc37991207b4505f7d6a653859bc0
françois hiriart <f.hiriart@skema.edu>
2021-06-12 17:21:22 +0200

    remove migration files and fixed images

--- db/migrate/20210612141412_drop_masks_table.rb
+++
@@ -1,5 +0,0 @@
-class DropMasksTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :masques
-  end
-end
--- db/migrate/20210612141424_drop_tables.rb
+++
@@ -1,8 +0,0 @@
-class DropTables < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :seringues
-    drop_table :catheters
-    drop_table :bandes
-    drop_table :scalpels
-  end
-end
